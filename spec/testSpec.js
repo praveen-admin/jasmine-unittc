bd2e33d58330d2101e8310dd35eb9d7378b1d015
praveen <praveen.opensource@gmail.com>
2016-11-16 14:28:05 +1100

    added basic test cases with test command as jasmine

---
+++ spec/testSpec.js
@@ -0,0 +1,16 @@
+/**
+ * Created by praveen on 16/11/2016.
+ */
+
+describe("suite",function () {
+   
+    describe("inner suite",function () {
+       it("test case",function () {
+           expect(true).toBe(true);
+       }) ;
+        it("test case",function () {
+            expect(true).toBe(false);
+        }) ;
+    });
+});
+    
